b0eba760adbc5cd3bd44b25b456b0ffc2e90c5d6
jayanth-caw <jayanth.reddy@caw.tech>
2025-06-03 07:51:33 +0530

    added new feature

---
+++ inc.js
@@ -0,0 +1,32 @@
+function inc(x) {
+    return x + 1;
+}
+
+function dec(x) {
+    return x - 1;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+
+
+console.log(inc(1));
+console.log(dec(1));
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
+console.log(div(1, 2));
