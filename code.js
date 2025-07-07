3c63b8ed61322f5d947d172290ca7391a9c369dc
jayanth-caw <jayanth.reddy@caw.tech>
2025-07-07 14:14:56 +0530

    added empty line

--- code.js
+++ code.js
@@ -105,3 +105,4 @@ function reverseWordsInSentence(str){
 // console.log(longestWord(['banana','water melon','cherry','date','apple','banana', 'apple']));
 // console.log(sumByReduce([1,2,3,4,5]));
 // console.log(expandString('a2b4c5'))
+// console.log(expandString('a2b4c5'))
